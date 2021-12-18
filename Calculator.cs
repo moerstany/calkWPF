4ba8ddbba3177f1e5d5bd223bf8d7778334505e9
Tanya <moerstany@gmail.com>
2021-12-18 16:50:15 +0700

    Рабочий :)

---
+++ Calculator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows;
+
+namespace CalkinWPF
+{
+
+
+    /// <summary>
+    /// Статический класс для вычесления арифметических действий
+    /// </summary>
+    public static class Calculator
+    {
+        /// <summary>
+        /// Подсчёт суммы двух чисел
+        /// </summary>
+        /// <param name="x">Первое число</param>
+        /// <param name="y">Второе число</param>
+        /// <returns>Сумма чисел</returns>
+        public static double Add(double x, double y) => x + y;
+
+        /// <summary>
+        /// Подсчёт разницы двух чисел
+        /// </summary>
+        /// <param name="x">Первое число</param>
+        /// <param name="y">Второе число</param>
+        /// <returns>Разница чисел</returns>
+        public static double Sub(double x, double y) => x - y;
+
+        /// <summary>
+        /// Подсчёт произведения двух чисел
+        /// </summary>
+        /// <param name="x">Первое число</param>
+        /// <param name="y">Второе число</param>
+        /// <returns>Произведение чисел</returns>
+        public static double Mult(double x, double y) => x * y;
+
+        /// <summary>
+        /// Подсчёт возведения в степень 
+        /// </summary>
+        /// <param name="x">Число</param>
+        /// <param name="y">степень</param>
+        /// <returns>Возведение в степень</returns>
+        public static double Pow1(double x, double y) => Math.Pow(x, y);
+
+        /// <summary>
+        /// Подсчёт извлечение квадратного корня
+        /// </summary>
+        /// <param name="x">Число</param>
+        /// <returns>Извлечение квадратного корня</returns>
+        public static double Sqrt1(double x) => Math.Sqrt(x);
+
+        /// <summary>
+        /// Подсчёт деления двух чисел
+        /// </summary>
+        /// <param name="x">Первое число</param>
+        /// <param name="y">Второе число</param>
+        /// <returns>Результат деления</returns>
+        /// <exception cref="DivideByZeroException">Если второе число равно 0</exception>
+        public static double Div(double x, double y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException("Делить на ноль нельзя!");
+            }
+
+            return x / y;
+        }
+
+
+    }
+}
+
+
+    
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalkinWPF
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        
+       
+        private void Start_OnClick_Sum(object sender, RoutedEventArgs e)
+        {   double x = Convert.ToDouble(Input1.Text);
+            double y = Convert.ToDouble(Input2.Text);
+
+            Otvet1.Content = Convert.ToString(Calculator.Add(x,y));
+            
+        }
+
+
+        private void Start_OnClick_Sub(object sender, RoutedEventArgs e)
+        {
+            double x = Convert.ToDouble(Input1.Text);
+            double y = Convert.ToDouble(Input2.Text);
+            
+            Otvet1.Content = Convert.ToString(Calculator.Sub(x,y));
+        }
+
+        private void Start_OnClick_Mult(object sender, RoutedEventArgs e)
+        {
+            double x = Convert.ToDouble(Input1.Text);
+            double y = Convert.ToDouble(Input2.Text);
+            
+            Otvet1.Content = Convert.ToString(Calculator.Mult(x,y));;
+        }
+
+        private void Start_OnClick_div(object sender, RoutedEventArgs e)
+        {
+            double x = Convert.ToDouble(Input1.Text);
+            double y = Convert.ToDouble(Input2.Text);
+            
+            Otvet1.Content = Convert.ToString(Calculator.Div(x,y));;
+        }
+
+        private void Start_OnClick_Pow1(object sender, RoutedEventArgs e)
+        {
+            double x = Convert.ToDouble(Input1.Text);
+            double y = Convert.ToDouble(Input2.Text);
+            
+            Otvet1.Content = Convert.ToString(Calculator.Pow1(x,y));
+        }
+
+        private void Start_OnClick_Sqrt1(object sender, RoutedEventArgs e)
+        {
+            double x = Convert.ToDouble(Input1.Text);
+           
+            
+            Otvet1.Content = Convert.ToString(Calculator.Sqrt1(x));
+        }
+    }
+}
+            
